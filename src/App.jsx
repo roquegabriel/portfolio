a92291073f3634cfe12328baea9b5e89f7a86968
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-06-18 14:40:51 -0400

    Add project scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  
+  return (
+  
+      <div>
+        <h1 className='text-5xl text-center'>Hi everyone!greeting from React and TailwindCSS </h1>
+      </div>
+  
+  )
+}
+
+export default App
