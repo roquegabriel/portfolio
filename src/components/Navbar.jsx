209664230b00609021deacb9e46903a62e18e9b6
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-06-19 09:34:26 -0400

    Add responsive navbar with social media buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Logo from '../assets/images/logo.png'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaFacebook, FaGithub, FaLinkedin, FaTimes } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 
 const Navbar = () => {
 
@@ -34,8 +36,24 @@ const Navbar = () => {
         <li className='py-6 text-4xl'>Works</li>
         <li className='py-6 text-4xl'>Contact</li>
       </ul>
+
       {/* Social icons */}
-      <div className='hidden'></div>
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">Linkdin <FaLinkedin size={30} /> </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">Github <FaGithub size={30} /> </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">Email <HiOutlineMail size={30} /> </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a className='flex justify-between items-center w-full text-gray-300' href="/">Resume <BsFillPersonLinesFill size={30} /> </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
