7ad14d08f36e2ada38185e202af72b2f894f8b22
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-06-19 13:36:35 -0400

    Add home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 
 function App() {
-  
+
   return (
-  
-      <div>
-        <Navbar />
-      </div>
-  
+
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { HiArrowNarrowRight } from 'react-icons/hi'
+
+
+const Home = () => {
+  return (
+    <div name="home" className='bg-[#0a192f] w-full h-screen'>
+      <div className='max-w-[1000px] h-full mx-auto flex flex-col justify-center px-8'>
+        <p className='text-pink-600'>Hi there, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Roque Pérez</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full stack developer with a passion for building intuitive and robust web solutions. Let's bring your vision to life with cutting-edge technology and innovative design.</p>
+        <div>
+          <button className='group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View works <span className='group-hover:rotate-90 duration-300'>
+            <HiArrowNarrowRight className='ml-3' />
+          </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
