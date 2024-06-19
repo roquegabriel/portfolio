0390a624b0cb3143a0671b922e53f6be83c8c385
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-06-18 22:32:53 -0400

    Set the font for body

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
   
       <div>
-        <h1 className='text-5xl text-center'>Hi everyone!greeting from React and TailwindCSS </h1>
+        <h1 className='text-5xl text-center'>Hi everyone! greeting from React and TailwindCSS </h1>
       </div>
   
   )
