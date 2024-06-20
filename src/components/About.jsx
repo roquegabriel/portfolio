7477b05b983ad5ff3d187b5fd96811f910c47213
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-06-19 21:12:10 -0400

    Update about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -16,7 +16,9 @@ const About = () => {
                         <p>Hi, I'm Roque, nice to meet you. Please take a look around</p>
                     </div>
                     <div>
-                        <p>I am passionate about building excellent software that improves the lives of those around me. I specialize in creating software for clients ranging from individuals and small-businesses all the way to large enterprise corporations. What would you do if you had a software expert available at your fingerptips?</p>
+                        <p>I'm a dedicated full stack developer with a passion for crafting seamless digital experiences. I specialize in both front-end and back-end development, bringing innovative ideas to life with clean, efficient code.
+                            Let's build something amazing together. Explore my work and reach out if you're ready to take your project to the next level!
+                        </p>
                     </div>
                 </div>
             </div>
