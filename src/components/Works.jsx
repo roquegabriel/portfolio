9a0c3c4c3e6ab1d6ca760f6d118ba5214130592b
roquegabriel <157759111+roquegabriel@users.noreply.github.com>
2024-07-27 00:08:18 -0400

    Correct texts

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -5,8 +5,8 @@ const Works = () => {
         <div name="works" className='w-full md:h-screen bg-midnight text-gray-300'>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
-                    <p className='text-4xl font-bold inline border-pink-600 border-b-4'>Work</p>
-                    <p className='py-4'>//Check out some of my recent work</p>
+                    <p className='text-4xl font-bold inline border-pink-600 border-b-4'>Works</p>
+                    <p className='py-4'>//Check out some of my recent works</p>
                 </div>
                 {/* Container */}
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
@@ -105,8 +105,6 @@ const Works = () => {
                     </div>
 
                 </div>
-
-
             </div>
         </div>
     )
